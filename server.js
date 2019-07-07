93180cc5f48bb8389f8316c7960ec8ca72b59b2c
danieloest <danieloest@users.noreply.github.com>
2019-07-06 22:30:07 -0600

    Working on pushing to heroku

--- server.js
+++ server.js
@@ -14,21 +14,21 @@ const userController = require("./controllers/userController.js");
 const { Pool } = require('pg');
 const pool = new Pool({connectionString: connectionString});
 
-var sql = "SELECT * FROM test";
+// var sql = "SELECT * FROM test";
 
-pool.query(sql, function(err, result) {
-    // If an error occurred...
-    if (err) {
-        console.log("Error in query: ")
-        console.log(err);
-    }
+// pool.query(sql, function(err, result) {
+//     // If an error occurred...
+//     if (err) {
+//         console.log("Error in query: ")
+//         console.log(err);
+//     }
 
-    // Log this to the console for debugging purposes.
-    console.log("Back from DB with result:");
-    console.log(result.rows);
+//     // Log this to the console for debugging purposes.
+//     console.log("Back from DB with result:");
+//     console.log(result.rows);
 
 
-});
+// });
 
 
 
